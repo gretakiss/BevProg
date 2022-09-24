9058cb8a3627d51b070e7ffe439af1c58ba2aa57
gretakiss <113306241+gretakiss@users.noreply.github.com>
2022-09-24 19:17:50 +0200

    Add files via upload

---
+++ hazik/hazi3szog.py
@@ -0,0 +1,16 @@
+def haromszog(name,name1,name3):
+    helyes = "A {},{} és {} oldalú háromszög szerkeszthető."
+    helytelen = "A {},{} és {} oldalú háromszög NEM szerkeszthető."
+    if a+b>=c and a+c>=b and b+c>=a:
+        print(helyes.format(a,b,c))
+    else:
+        print(helytelen.format(a,b,c))
+
+
+if __name__ == "__main__":
+    print("Adja meg a háromszög három oldalát cm-ben:")
+    a = int(input("a oldal (cm): "))
+    b = int(input("b oldal (cm): "))
+    c = int(input("c oldal (cm): "))
+    haromszog(a,b,c)
+
